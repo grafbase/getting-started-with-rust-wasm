a838c803af680d711fef838fd436f2fc5e68b01b
Yoav Lavi <yoav@yoavlavi.com>
2023-07-11 17:12:55 +0300

    use mjs webpack config

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,8 +1,12 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import WasmPackPlugin from "@wasm-tool/wasm-pack-plugin";
+import { fileURLToPath } from "url";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: "./src/bundler.js",
   output: {
     path: path.resolve(__dirname, "dist"),
