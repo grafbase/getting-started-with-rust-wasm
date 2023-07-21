6523d984116fecfdba7939f24e14f537244fe734
Yoav Lavi <yoav@yoavlavi.com>
2023-07-21 15:34:48 +0300

    simplify flake

--- flake.nix
+++ flake.nix
@@ -14,55 +14,47 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
 
   outputs =
     { self
     , nixpkgs
     , flake-utils
-    , rust-overlay
     , ...
     }:
     let
       inherit (nixpkgs.lib) optional;
       systems = flake-utils.lib.system;
     in
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ (import rust-overlay) ];
-      };
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              cargo-make
+              deno
+              nodejs
+              nodePackages.prettier
+              rustup
+              wasm-pack
+            ]
+            ++ optional (system == systems.aarch64-darwin) [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.CoreFoundation
+              darwin.apple_sdk.frameworks.CoreServices
+            ];
 
-      rustToolChain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      defaultShellConf = {
-        nativeBuildInputs = with pkgs;
-          [
-            cargo-make
-            deno
-            nodejs
-            nodePackages.prettier
-            rustup
-            wasm-pack
-          ]
-          ++ optional (system == systems.aarch64-darwin) [
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.CoreFoundation
-            darwin.apple_sdk.frameworks.CoreServices
-          ];
-
-        shellHook = ''
-          export CARGO_INSTALL_ROOT="$(git rev-parse --show-toplevel)/.cargo"
-          export PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
-        '';
-      };
-    in
-    { devShells.default = pkgs.mkShell defaultShellConf; });
+          shellHook = ''
+            export CARGO_INSTALL_ROOT="$(git rev-parse --show-toplevel)/.cargo"
+            export PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
+          '';
+        };
+      });
 }
+        
